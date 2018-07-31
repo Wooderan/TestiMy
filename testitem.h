87dc29a7b505de8ff8c3d6f1b5cdc397abaaf04f
Wooderan <eugentopolevski@gmail.com>
2018-07-31 05:58:14 +0300

    Adding testitem realisation!

--- testitem.h
+++ testitem.h
@@ -1,11 +1,27 @@
 #ifndef TESTITEM_H
 #define TESTITEM_H
 
+#include <QString>
+#include <vector>
+
+struct Variant{
+    Variant(bool _correct, QString _answer);
+    bool correct;
+    QString answer;
+};
 
 class TestItem
 {
+protected:
+    QString question;
+    std::vector<Variant> variants;
 public:
-    TestItem();
+    TestItem(QString _str, int _n);
+    const QString &getQuestion() const;
+    void setQuestion(const QString &value);
+    const Variant &at(size_t n);
+    void addVar(bool _correct, QString _answ);
+    size_t getN();
 };
 
 #endif // TESTITEM_H
